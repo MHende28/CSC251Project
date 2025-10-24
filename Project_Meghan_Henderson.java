0c75fc192a489d2551798b2e0d632d8221ada396
MHende28 <mhende28@email.cpcc.edu>
2025-10-24 14:42:10 -0400

    Project_Meghan_Henderson Finished
    Added code to display all policy information the number of smokers, and the number of non-smokers

--- Project_Meghan_Henderson.java
+++ Project_Meghan_Henderson.java
@@ -1,11 +1,10 @@
+// Import statments
 import java.util.Scanner;
 import java.io.*;
 import java.util.ArrayList;
 
-public class Project_Meghan_Henderson 
-{   
-   public static void main(String[] args)
-   {
+public class Project_Meghan_Henderson {   
+   public static void main(String[] args) {
    
      try {
       
@@ -22,48 +21,78 @@ public class Project_Meghan_Henderson
       double height = 0.0;
       double weight = 0.0;
       String fileInput = "";
+      int totalSmokers = 0;
+      int totalNonSmokers = 0;
+      
       
       //Create an array list to store objects. The ArrayList will hold Policy objects.
       ArrayList<Policy> policyAccounts = new ArrayList<Policy>();
-
-      while (inputFile.hasNext()) {
-      
-         policyNumber = inputFile.nextLine();
-         providerName = inputFile.nextLine();
-         firstName = inputFile.nextLine();
-         lastName = inputFile.nextLine();
+            
+            //While loop to read file and set them to proper variable
+            while (inputFile.hasNext()) {
          
-         fileInput = inputFile.nextLine();
-         age = Integer.parseInt(fileInput);
-         
-         smokingStatus = inputFile.nextLine();
-         
-         fileInput = inputFile.nextLine();
-         height = Double.parseDouble(fileInput);
-         
-         fileInput = inputFile.nextLine();
-         weight = Double.parseDouble(fileInput);
-         
-         if (inputFile.hasNext()) {
-         
-         inputFile.nextLine();
+            policyNumber = inputFile.nextLine();
+            providerName = inputFile.nextLine();
+            firstName = inputFile.nextLine();
+            lastName = inputFile.nextLine();
+            
+            fileInput = inputFile.nextLine();
+            age = Integer.parseInt(fileInput);
+            
+            smokingStatus = inputFile.nextLine();
+            
+            fileInput = inputFile.nextLine();
+            height = Double.parseDouble(fileInput);
+            
+            fileInput = inputFile.nextLine();
+            weight = Double.parseDouble(fileInput);
+            
+            if (inputFile.hasNext()) {
+               inputFile.nextLine();
+            }
+            
+            Policy p = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+            
+            policyAccounts.add(p);         
          }
          
-         Policy p = new Policy(policyNumber, providerName, firstName, lastName, age, smokingStatus, height, weight);
+         inputFile.close();
          
-         policyAccounts.add(p);         
+         
+         //Print output
+         for (int index = 0; index < policyAccounts.size(); index++) {
+   
+            System.out.println("Policy Number: " + policyAccounts.get(index).getPolicyNumber());
+            System.out.println("Provider Name: " + policyAccounts.get(index).getProviderName());
+            System.out.println("Policyholder's First Name: " + policyAccounts.get(index).getFirstName());
+            System.out.println("Policyholder's Last Name: " + policyAccounts.get(index).getLastName());
+            System.out.println("Policyholder's Age: " + policyAccounts.get(index).getAge());
+            System.out.println("Policyholder's Smoking Status: " + policyAccounts.get(index).getSmokingStatus());
+            System.out.println("Policyholder's Height: " + policyAccounts.get(index).getHeight());
+            System.out.println("Policyholder's Weight: " + policyAccounts.get(index).getWeight());
+            System.out.printf("Policyholder's BMI: %.2f\n", policyAccounts.get(index).getBMI());
+            System.out.printf("Policy Price: $%.2f\n", policyAccounts.get(index).getPrice());
+            System.out.println();
+            
+            //accumulate somker and non-smokers
+            if (policyAccounts.get(index).getSmokingStatus().equalsIgnoreCase("smoker")) {
+               totalSmokers++;
+           } 
+           
+            else {
+               totalNonSmokers++;
+           }         
+         }
+         
+         //amount of somker and non-smokers output
+         System.out.println("The number of policies with a smoker is: " + totalSmokers);
+         System.out.println("The number of policies with a non-smoker is: " + totalNonSmokers);
+         
       }
+         //Error is file not found correctly
+         catch(IOException ex) {
+            System.out.println("Something went wrong reading the file: " + ex.getMessage());
+         }
       
-      inputFile.close();
-      
-      }
-      
-      catch(IOException ex)
-      {
-      
-         System.out.println("Something went wrong reading the file: " + ex.getMessage());
-      }
-
-       
    }
 }
