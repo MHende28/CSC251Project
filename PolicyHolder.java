a4612f280b3f010710ccd13c31648260d9b42ed6
MHende28 <mhende28@email.cpcc.edu>
2025-11-01 13:07:56 -0400

    updated PolicyHolder.java
    Added code to implicitly call toString method and output information about Policy and PolicyHolders

--- PolicyHolder.java
+++ PolicyHolder.java
@@ -19,6 +19,16 @@ public class PolicyHolder {
       holdersWeight = weight;
    }
    
+   public PolicyHolder(PolicyHolder object2) {
+      
+      firstName = object2.firstName;
+      lastName = object2.lastName;
+      holdersAge = object2.holdersAge;
+      smokingStatus = object2.smokingStatus;
+      holdersHeight = object2.holdersHeight;
+      holdersWeight = object2.holdersWeight;
+   }
+   
    public void setFirstName(String first) {
       
       firstName = first;
@@ -97,12 +107,50 @@ public class PolicyHolder {
       
    }
    
+   /**
+      The getPrice method calculates the Policy's price.
+      @return calcualted policy price with fees.
+   */
+   public double getPrice()
+   {
+      final double BASE_PRICE = 600;
+      final double ADDITIONAL_FEE_AGE = 75;
+      final double ADDITIONAL_FEE_SMOKING = 100;
+      final double ADDITIONAL_FEE_PER_BMI = 20;
+      
+      final int AGE_THRESHOLD = 50;
+      final int BMI_THRESHOLD = 35;
+      
+      double price = BASE_PRICE;
+      
+      if(holdersAge > AGE_THRESHOLD) //over 50 years
+         price += ADDITIONAL_FEE_AGE; //75
+         
+      if(smokingStatus.equalsIgnoreCase("smoker")) 
+         price += ADDITIONAL_FEE_SMOKING; //100
+            
+      //call the getBMI method
+      if(getBMI() > BMI_THRESHOLD) //BMI over 35
+         price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI); //additional BMI fee - 20
+         
+      return price;
+   }
+   
    public String toString() {
-      
-      String str = "Holder First Name: " + firstName + "\nHolders Last Name: " + lastName +
-                   "\nHolders Age: " + holdersAge + "\nHolders Smoking Status: " + smokingStatus +
-                   "\nHolders Height: " + holdersHeight + "\nHolders Weight: " + holdersWeight;
-      
-      return str;
+      String str = String.format(
+        "Policyholder's First Name: %s%n" +
+        "Policyholder's Last Name: %s%n" +
+        "Policyholder's Age: %d%n" +
+        "Policyholder's Smoking Status: %s%n" +
+        "Policyholder's Height: %.1f inches%n" +
+        "Policyholder's Weight: %.1f pounds%n" +
+        "Policyholder's BMI: %.2f%n" +
+        "Policy Price: $%.2f",
+        firstName, lastName, holdersAge, smokingStatus,
+        holdersHeight, holdersWeight, this.getBMI(), this.getPrice()
+    );
+
+    return str;
+   
    }
 }
